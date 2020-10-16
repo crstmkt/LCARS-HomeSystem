dba1ae3c5c7f2efba376dc14bf8cb4eba402a0a4
Christian <c.mertenskoetter@live.com>
2020-10-16 11:13:58 +0200

    Token

---
+++ js/Token.js
@@ -0,0 +1,3 @@
+function getToken(){
+    return("");
+}
--- js/WebSocket.js
+++ js/WebSocket.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     ws = new WebSocket('ws://homeassistant:8123/api/websocket');
 
     ws.addEventListener('open', function (event) {
-      ws.send('{"type": "auth","access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYmVjYTgwYTYzMDU0MjM3ODJkYmE1YmQyYmRhMjBjNyIsImlhdCI6MTYwMjc1ODU3MCwiZXhwIjoxOTE4MTE4NTcwfQ.tTO4QCcQsNrHTv0Li7dh5gM33PDDrn28bxZfRzIO1Y0"}\n');
+      ws.send('{"type": "auth","access_token": '+ getToken() +'}\n');
       ws.send('{"id": 1, "type": "subscribe_events", "event_type": "state_changed"}\n');
     });
 
