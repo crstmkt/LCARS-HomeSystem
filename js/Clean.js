61f6a9c6a91c96d4bd32894d9453095f1b20bde4
Christian <c.mertenskoetter@live.com>
2021-01-09 12:17:21 +0100

    New API.js moved getToken to API.js new function getHost()

--- js/Clean.js
+++ js/Clean.js
@@ -1,7 +1,7 @@
 function startClean(){
     $.ajax({
         type: 'POST',
-        url: 'http://homeassistant:8123/api/services/vacuum/start',
+        url: getHost() + '/api/services/vacuum/start',
         data: '{"entity_id": "vacuum.lcars_clean"}',
         headers: {
             'Authorization': 'Bearer ' + getToken(),
