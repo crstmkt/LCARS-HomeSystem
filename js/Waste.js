756d38846bf621db39b4f1efd9aea83374579cea
Christian <c.mertenskoetter@live.com>
2021-01-31 12:32:24 +0100

    Added Waste Collection Module (API)

---
+++ js/Waste.js
@@ -0,0 +1,36 @@
+function initiateWasteCollection(){
+	getNextWasteCollection();
+	setInterval(function(){getNextWasteCollection();}, 3600000)
+}
+
+function getNextWasteCollection(){
+	apiCallWasteCollectionDate();
+	apiCallWasteCollectionType();
+}
+
+function apiCallWasteCollectionType(){
+	$.ajax({
+			url: getHost() + '/api/states/sensor.next_waste_collection_type',
+			headers: {
+				'Authorization': 'Bearer ' + getToken()
+			},
+			success: function(data){
+				$('#nextWasteCollectionType').html('<p>: ' + data.state.toUpperCase() + '</p>');
+			}
+		})
+}
+
+function apiCallWasteCollectionDate(){
+	$.ajax({
+			url: getHost() + '/api/states/sensor.next_waste_collection_date',
+			headers: {
+				'Authorization': 'Bearer ' + getToken()
+			},
+			success: function(data){
+				$('#nextWasteCollectionDate').html('<p>' + data.state + '</p>');
+			}
+		})
+}
+//http://lcars-homesystem:8123/api/states/sensor.next_waste_collection_date
+//http://lcars-homesystem:8123/api/states/sensor.next_waste_collection_type
+//
