5364920e142ddebaf8324e56db717f4ebaaf18c3
Christian <c.mertenskoetter@live.com>
2021-03-26 19:37:19 +0100

    Added WiFi

--- module.js
+++ module.js
@@ -46,9 +46,9 @@ children:[
                     //Header Round Button Group
                     {type:'wrapper', flex:'h', version:'button-wrap', children:[
                         {type:'button', color:'bg-grey-4', version:'round', label:'GREY MODE', href:'javascript:activateGreyMode();'},
-                        {type:'button', color:'bg-blue-2', version:'round', label: 'TEST', href:''},
+                        {type:'button', color:'bg-blue-2', version:'round', label: 'RELOAD', href:'javascript:window.location.reload(true);'},
                         {type:'button', id:'guestWifiSwitch', color: 'bg-orange-4', version:'round', label: 'GUEST WIFI', href:'javascript:toggleGuestWifi();'},
-                        {type:'button', color:LCARS.helper.aRandColor(uiColors), version:'round'},
+                        {type:'button', color:LCARS.helper.aRandColor(uiColors), version:'round', label:'Test', href:'javascript:changeHub();'},
                         {type:'button', color:LCARS.helper.aRandColor(uiColors), version:'round', label:'clean', href:'javascript:startClean();'},
                         {type:'button', color:LCARS.helper.aRandColor(uiColors), version:'round'}
                     ]}
@@ -76,12 +76,12 @@ children:[
         //Left Columns & Elbow
         {type:'wrapper', version:'column', flex:'v', children:[
             {type:'elbow', version:'horizontal', direction:'top-left', color:LCARS.helper.aRandColor(uiColors), class:'step-two', hidden: true},
-            {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, label: 'DASH'},
+            {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, label: 'DASHBOARD'},
             {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, size:'step-two'},
             {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true},
             {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, size:'step-two'},
             {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, flexc:'v'},
-            {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true},
+            {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, label: 'WIFI'},
             {type:'button', color:LCARS.helper.aRandColor(uiColors), hidden: true, size: 'step-two'}
         ]},
 
@@ -98,28 +98,63 @@ children:[
             ]},
 		
             //Start Main Content Wrapper
-            {type:'wrapper', id:'wpr_centerControls' , style:{'overflow':'auto'}, version:'content', flex:'h', flexc:'v',children:[
+            {type:'wrapper', id:'wpr_centerControls' ,  version:'content', flex:'h', flexc:'v',children:[
 				
-				//WasteCollectionLeft
-				{type:'wrapper', version:'column', flex:'v', children:[
-					{type:'elbow', version:'horizontal', direction:'top-left', color:LCARS.helper.aRandColor(uiColors), class:'step-two', hidden: true},
-					{type:'block', color:LCARS.helper.aRandColor(uiColors), hidden: true, label: 'WASTE'}
-				]},
-				//WasteCollectionTop
-				{type:'column', flexc: 'h', flex:'v', children:[
-					{type:'row', flex:'h', version:'frame', children:[
-						{type:'title', size: 'small', text:'NEXT WASTE COLLECTION', hidden: true},
-						{type:'cap', version:'round-right', color:LCARS.helper.aRandColor(uiColors), hidden: true, size: 'small'}
-					]},
-					//WasteCollectionContentWrapper
-					{type:'wrapper', style:{'overflow':'auto'}, version:'content', flex:'h', flexc:'v',children:[
-						{type:'text', size: 'large', id: 'nextWasteCollectionDate', color:LCARS.helper.aRandColor(uiColors)},
-						{type:'text', size: 'large', id: 'nextWasteCollectionType', color:LCARS.helper.aRandColor(uiColors)}
-					]}
-					
-				]}
-				
-				
+                {type: 'wrapper', id: 'dashboard', version: 'content', flex:'h', flexc:'v', hidden: true, children:[
+				    //WasteCollectionLeft
+				    {type:'wrapper', version:'column', flex:'v', children:[
+				    	{type:'elbow', version:'horizontal', direction:'top-left', color:LCARS.helper.aRandColor(uiColors), class:'step-two', hidden: true},
+				    	{type:'block', color:LCARS.helper.aRandColor(uiColors), hidden: true, label: 'WASTE'}
+				    ]},
+				    //WasteCollectionTop
+				    {type:'column', flexc: 'h', flex:'v', children:[
+				    	{type:'row', flex:'h', version:'frame', hidden: true, children:[
+				    		{type:'title', size: 'small', text:'NEXT WASTE COLLECTION',},
+				    		{type:'cap', version:'round-right', color:LCARS.helper.aRandColor(uiColors), size: 'small'}
+				    	]},
+				    	//WasteCollectionContentWrapper
+				    	{type:'wrapper', version:'content', flex:'h', flexc:'v', hidden: true, children:[
+                            {type: 'wrapper', flex: 'h', flexc: 'v', children: [
+                                {type: 'text', id: 'nextWasteCollection', color: 'text-blue-1', text: ''}
+                            ]}
+				    	]}
+				    ]},
+                    //WasteCollection End
+
+                    //Wifi Start
+                    {type:'wrapper', flex: 'h', flexc: 'v' , children:[
+                        {type:'defaultBracket', namespace:'sdk', 
+                            coloring:{
+                                elbow:LCARS.helper.aRandColor(uiColors),
+                                column1:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                column2:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                column3:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                column4:LCARS.helper.aRandColorGroup(uiColors, 3),
+                            },
+                            content:[
+                                {type: 'title', color: LCARS.helper.aRandColor(uiColors), text: 'SSID: LCARS_Net_g'}       
+                            ]}
+                    ]}
+                    //Wifi End
+
+                ]},
+
+                {type: 'wrapper', id: 'wifi', version: 'content', flex:'h', flexc:'v', hidden: true, children:[
+                    //{type: 'title', color: LCARS.helper.aRandColor(uiColors), text: 'SSID: LCARS_Net_g'},
+                        {type:'wrapper', children:[
+                            {type:'defaultBracket', namespace:'sdk', 
+                                coloring:{
+                                    elbow:LCARS.helper.aRandColor(uiColors),
+                                    column1:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                    column2:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                    column3:LCARS.helper.aRandColorGroup(uiColors, 3),				
+                                    column4:LCARS.helper.aRandColorGroup(uiColors, 3),
+                                },
+                                content:[
+                                    {type: 'title', color: LCARS.helper.aRandColor(uiColors), text: 'SSID: LCARS_Net_g'}       
+                                ]}
+                        ]}
+                ]}
 			]}
 			//End Main Content Wrapper
         ]}
