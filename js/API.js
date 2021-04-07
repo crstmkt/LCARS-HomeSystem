41c6d90bd5e0c1369590ce2c45c91a25e31bf666
crstmkt <c.mertenskoetter@live.com>
2021-04-07 12:15:54 +0200

    Update API.js
    removed token

--- js/API.js
+++ js/API.js
@@ -1,5 +1,5 @@
 function getToken(){
-    return('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYmVjYTgwYTYzMDU0MjM3ODJkYmE1YmQyYmRhMjBjNyIsImlhdCI6MTYwMjc1ODU3MCwiZXhwIjoxOTE4MTE4NTcwfQ.tTO4QCcQsNrHTv0Li7dh5gM33PDDrn28bxZfRzIO1Y0');
+    return('<Token goes HERE>');
 }
 
 function getHost(){
