7faeaef580ad92fc21b5975433668043805c476b
crstmkt <c.mertenskoetter@live.com>
2021-07-29 09:17:17 +0200

    preps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
