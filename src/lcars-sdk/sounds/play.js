cf4f01bb2ca629dd49190757a68ece1e4ec9dc88
crstmkt <c.mertenskoetter@live.com>
2021-08-02 15:02:47 +0200

    Bunch of fixes and Hardcoded UI. Has to be imrpoved to flexible JS UI

--- src/lcars-sdk/sounds/play.js
+++ src/lcars-sdk/sounds/play.js
@@ -1,32 +1,31 @@
-$( document ).ready(function() {
-    var audio = new Audio('/lcars-sdk/sounds/0003.wav');
+//TO DO: Improve this function
+// $(document).ready(function () {
+//   var audio = new Audio("/lcars-sdk/sounds/0003.wav");
 
-    $(".slider").on("mousedown", () => {
-        audio.play()
-        setTimeout(() => {
-            console.log(1, audio.currentTime)
-          audio.currentTime = 0.15;
-          timer = setInterval(() => {
-              console.log(2, audio.currentTime)
-              audio.currentTime = 0.3;
-          }, 500); 
-        }, 750);
-      }).on("mouseup", () => {
-        clearInterval(timer);
-      })
-});
+//   $(".slider")
+//     .on("mousedown", () => {
+//       audio.play();
+//       setTimeout(() => {
+//         console.log(1, audio.currentTime);
+//         audio.currentTime = 0.15;
+//         timer = setInterval(() => {
+//           console.log(2, audio.currentTime);
+//           audio.currentTime = 0.3;
+//         }, 500);
+//       }, 750);
+//     })
+//     .on("mouseup", () => {
+//       clearInterval(timer);
+//     });
+// });
 
-function play(sound) {
-    var audio;
-    
-    switch(sound){
-        case 0001:
-            audio = new Audio('/lcars-sdk/sounds/0001.wav');
-            audio.play();
-        break;
-        case 0002:
-            audio = new Audio('/lcars-sdk/sounds/0002.wav');
-            audio.play();
-        break;
-      }
+export default function play(sound) {
+  switch (sound) {
+    case 1:
+      new Audio("/lcars-sdk/sounds/0001.wav").play();
+      break;
+    case 2:
+      new Audio("/lcars-sdk/sounds/0002.wav").play();
+      break;
   }
+}
