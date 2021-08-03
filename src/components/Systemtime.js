9cfaea7b201848f97180e298fd44406c73e30b4f
crstmkt <c.mertenskoetter@live.com>
2021-08-03 07:53:38 +0200

    fixes on systemtime

--- src/components/Systemtime.js
+++ src/components/Systemtime.js
@@ -1,17 +1,26 @@
 import moment from "moment";
 import $ from "jquery";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Systemtime = () => {
+  const [systemtime, setSystemtime] = useState(
+    moment().format("DD.MM.YYYY hh:mm:ss")
+  );
+
   useEffect(() => {
     setInterval(() => {
-      $("#systemTime").html(
-        "SYSTEM TIME " + moment().format("DD.MM.YYYY hh:mm:ss")
+      setSystemtime(
+        (prevSystemtime) =>
+          (prevSystemtime = moment().format("DD.MM.YYYY hh:mm:ss"))
       );
     }, 1000);
   });
 
-  return <div id="systemTime" class="title half"></div>;
+  return (
+    <div id="systemTime" class="title half">
+      {systemtime}
+    </div>
+  );
 };
 
 export default Systemtime;
